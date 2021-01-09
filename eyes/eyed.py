420ffbd627eda5d9b3d18e6decda9ad3650763d6
Devesh Saxena <75259875+devesh0605@users.noreply.github.com>
2021-01-09 23:01:32 +0530

    Update eyed.py

--- eyes/eyed.py
+++ eyes/eyed.py
@@ -1,9 +1,11 @@
+#importing libraries
 import cv2
 import numpy as np
-
+#loading haarcascade file
 cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
+#loading image
 img = cv2.imread('goodboy.jpg')
+#resizing image
 img = cv2.resize(img, (500, 500))
 copy = img.copy()
 gray = cv2.cvtColor(copy,cv2.COLOR_BGR2GRAY)
@@ -12,5 +14,6 @@ for (ex,ey,ew,eh) in eyes:
     cv2.rectangle(copy,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
 stack=np.hstack([img,copy])
+#shoing image
 cv2.imshow('Output',stack)
 cv2.waitKey(0)
